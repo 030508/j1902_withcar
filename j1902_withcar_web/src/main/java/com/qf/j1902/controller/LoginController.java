0dcc30598ff6fda6592f77584bc20fa85f0a38fa
git <1554148254@qq.com>
2019-07-06 13:47:08 +0800

    2019/7/6

---
+++ j1902_withcar_web/src/main/java/com/qf/j1902/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.qf.j1902.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/main")
+    public String main(){
+        return "main";
+    }
+}
